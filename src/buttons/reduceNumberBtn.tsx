14231f9f32916be54f2f50d410b560c1292331a0
ss77995ss <ss77995ss@gmail.com>
2019-11-23 18:19:26 +0800

    Implement buttons for manipulate the box rows

---
+++ src/buttons/reduceNumberBtn.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+interface propsType {
+  initialValue: number;
+}
+
+const ReduceNumberBtn: React.SFC<propsType> = ({ initialValue }) => {
+  const [value, setValue] = useState(initialValue);
+  const handleClick = () => {
+    if (value === 0) return;
+
+    setValue(value - 1);
+  };
+
+  return <button onClick={handleClick}>+</button>;
+};
+
+export default ReduceNumberBtn;
